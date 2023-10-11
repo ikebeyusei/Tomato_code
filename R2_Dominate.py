a469adc4e070d333e590ab6cc4a5dce2b0bde964
池辺 <84576633+ikebeyusei@users.noreply.github.com>
2023-10-11 16:04:58 +0900

    Update R2_Dominate.py

--- R2_Dominate.py
+++ R2_Dominate.py
@@ -6,7 +6,8 @@ import smach_ros
 import time
 from sensor_msgs.msg import Joy
 
-madeCounter = 1
+global madeCounter
+modeCounter = 1
 wasButtonPush = False
 
 class JoyTwist(object):
@@ -25,33 +26,35 @@ class JoyTwist(object):
         else:
             wasButtonPush = False
 
-        if modeCounter == 51:
+        if modeCounter == 9:
             modeCounter = 1
 
 #move_mode
 class Stop(smach.State):
     #process everything through "Stop"
     def __init__(self):
-        smach.State.__init__(self,outcomes=['move_forward','step_back','turn_left','turn_right','move_to_the_right','move_to_the_left','doing'])
+        smach.State.__init__(self,outcomes=['move_forward','step_back','turn_left','turn_right','move_to_the_right','move_to_the_left','doing','done'])
 
     def execute(self,userdata):
         global robot_mode
         robot_mode = 0
         #when modeCounter is 2, R2 tries to keep stopping
-        if modeCounter == 2:
+        if modeCounter == 1:
             return 'doing'
-        elif modeCounter == 3:
+        elif modeCounter == 2:
             return 'move_forward'
-        elif modeCounter == 4:
+        elif modeCounter == 3:
             return 'step_back'
-        elif modeCounter == 5:
+        elif modeCounter == 4:
             return 'turn_left'
-        elif modeCounter == 6:
+        elif modeCounter == 5:
             return 'turn_right'
-        elif modeCounter == 7:
+        elif modeCounter == 6:
             return 'move_to_the_right'
-        elif modeCounter == 8:
+        elif modeCounter == 7:
             return 'move_to_the_left'
+        else:
+            return 'done'
         
 class Move_forward(smach.State):
     def __init__(self):
@@ -167,7 +170,7 @@ class Line_tracing(smach.State):
         if detection_line == True:
             return 'detection'
         else:
-            return 'not_detection'
+            return 'not_detected'
 
     
 def main():
@@ -176,26 +179,27 @@ def main():
     with sm:
         init_sub = smach.StateMachine(outcomes=['Not','doing'])
         with init_sub:
-            smach.StateMachine.add('Line_Tracing',Line_tracing(),transitions={'detection':'Line_Tracing','not_detected':'doing'})
+            smach.StateMachine.add('Line_Tracing',Line_tracing(),transitions={'detection':'Line_Tracing','not_detected':'Not'})
         smach.StateMachine.add('LINE_TRACING',init_sub,transitions={'Not':'MOVE_MODE','doing':'LINE_TRACING'})
 
-        move_sub = smach.StateMachine(outcomes = ['mode_finish'])
+        move_sub = smach.StateMachine(outcomes = ['done'])
         with move_sub:
-            smach.StateMachine.add('Stop!',Stop(),transitions={'doing':'Stop!','move_forward':'Move_Forward','step_back':'Step_Back','turn_left':'Turn_Left','turn_right':'Turn_Right','move_to_the_right':'Move_To_The_Right','move_to_the_left':'Move_To_The_Left'})
+            smach.StateMachine.add('Stop!',Stop(),transitions={'doing':'Stop!','move_forward':'Move_Forward','step_back':'Step_Back','turn_left':'Turn_Left','turn_right':'Turn_Right','move_to_the_right':'Move_To_The_Right','move_to_the_left':'Move_To_The_Left','done':'done'})
             smach.StateMachine.add('Move_Forward',Move_forward(),transitions={'stop':'Stop!'})
             smach.StateMachine.add('Step_Back',Step_back(),transitions={'stop':'Stop!'})
             smach.StateMachine.add('Turn_Left',Turn_left(),transitions={'stop':'Stop!'})
             smach.StateMachine.add('Turn_Right',Turn_right(),transitions={'stop':'Stop!'})
             smach.StateMachine.add('Move_To_The_Right',Move_to_the_right(),transitions={'stop':'Stop!'})
             smach.StateMachine.add('Move_To_The_Left',Move_to_the_left(),transitions={'stop':'Stop!'})
-        smach.StateMachine.add('MOVE_MODE',move_sub,transitions={'mode_finish':'ARM_MODE'})
+        smach.StateMachine.add('MOVE_MODE',move_sub,transitions={'done':'ARM_MODE'})
 
         arm_sub = smach.StateMachine(outcomes = {'mode_finish'})
         with arm_sub:
             smach.StateMachine.add('Arm_Init',Arm_init(),transitions={'done':'Arm_Open'})
             smach.StateMachine.add('Arm_Opne',Arm_open(),transitions={'done':'Arm_Close'})
-            smach.StateMachine.add('Arm_Close',Arm_close(),transitions={'done':'MOVE_MODE'})
-
+            smach.StateMachine.add('Arm_Close',Arm_close(),transitions={'done':'mode_finish'})
+        smach.StateMachine.add('ARM_MODE',arm_sub,transitions={'mode_finish':'MOVE_MODE'})
+        
     sis = smach_ros.IntrospectionServer('smach_server', sm, '/ROOT')
     sis.start()
     outcome = sm.execute()
